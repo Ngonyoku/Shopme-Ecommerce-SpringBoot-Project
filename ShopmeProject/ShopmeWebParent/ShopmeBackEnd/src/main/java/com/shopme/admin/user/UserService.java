1801546edc3ceac545eff37f654e78baf3f1f6ab
Ngonyoku <ngonyoku001@gmail.com>
2022-01-20 15:58:27 +0300

    Password Encryption

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -3,6 +3,7 @@ package com.shopme.admin.user;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.shopme.common.entity.Role;
@@ -17,6 +18,9 @@ public class UserService {
 	@Autowired
 	private RoleRepository roleRepository;
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	/*
 	 * Returns a list of all users
 	 * */
@@ -28,7 +32,18 @@ public class UserService {
 		return (List<Role>) roleRepository.findAll();
 	}
 	
+	/*
+	 * Save User Data to the database
+	 * */
 	public void save(User user) {
+		encodePasword(user);
 		userRepository.save(user);
+	}
+	
+	public void encodePasword(User user) {
+		String encodedPassword = passwordEncoder.encode(user.getPassword());
+		//Updated the User password with the new encoded password
+		user.setPassword(encodedPassword);
+		
 	}
 }
